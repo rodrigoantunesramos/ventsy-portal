196c1a5c360d9db8d102cb76e54e31e5341e4a32
rodrigoantunesramos <rodrigoantunesramos@gmail.com>
2026-03-18 18:32:47 -0300

    Update config.js

--- ventsy-dashboard/js/config.js
+++ ventsy-dashboard/js/config.js
@@ -6,4 +6,4 @@ export const SUPA_URL = 'https://hxvlfalgrduitevbhqvq.supabase.co';
 export const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4dmxmYWxncmR1aXRldmJocXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMDA2MDYsImV4cCI6MjA4ODc3NjYwNn0.AUUY8k1dM3rzanf6qdiqk9kcFztDFFm-SuEv2aoBbQQ';
 
 // Rotas válidas da SPA
-export const ROUTES = ['dashboard', 'financeiro', 'documentos', 'equipe', 'calendario','fotos', 'minhapropriedade'];
+export const ROUTES = ['dashboard', 'financeiro', 'documentos', 'equipe', 'calendario','fotos', 'minhapropriedade', 'leads'];
